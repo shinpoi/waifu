9fdaedf52bee0c8fb640c59478d1e663abb838ac
aoi-lucario <petersbw123@gmail.com>
2020-02-04 09:30:04 +0900

    set base dir

--- extra/rumia39/animation.sh
+++ extra/rumia39/animation.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+BASE=$(dirname "$0")
 slp=${1:-0.15}
 
 while true; do
   for i in {1..39}; do
-    cat flash/$i;
+    cat $BASE/flash/$i;
     sleep $slp;
     printf '\033[34A';
   done
